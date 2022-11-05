f6faf6978093c9b5bae39521ddb51c8949d42f3e
cami <camimcp21@gmail.com>
2022-11-05 09:55:58 -0500

    registro

--- techdesign.sql
+++ techdesign.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 29-10-2022 a las 13:57:53
+-- Tiempo de generación: 05-11-2022 a las 14:55:35
 -- Versión del servidor: 5.7.31
 -- Versión de PHP: 7.3.21
 
@@ -24,6 +24,40 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `productos`
+--
+
+DROP TABLE IF EXISTS `productos`;
+CREATE TABLE IF NOT EXISTS `productos` (
+  `id_p` int(11) NOT NULL AUTO_INCREMENT,
+  `producto` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `precio` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  PRIMARY KEY (`id_p`)
+) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `productos`
+--
+
+INSERT INTO `productos` (`id_p`, `producto`, `precio`, `cantidad`) VALUES
+(1, 'jabon', 2000, 3),
+(2, 'olla', 20, 88),
+(3, 'jabon', 2000, 3),
+(4, 'olla', 2000, 3),
+(5, 'olla', 2000, 3),
+(6, 'olla', 2000, 3),
+(7, 'jabon', 20, 88),
+(8, 'olla', 2000, 3),
+(9, 'df', 34, 34),
+(10, 'olla', 20, 545),
+(11, 'olla', 20, 3),
+(12, 'olla', 2000, 3),
+(13, 'jabon', 20, 3);
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `user`
 --
 
@@ -36,7 +70,26 @@ CREATE TABLE IF NOT EXISTS `user` (
   `correo` varchar(50) NOT NULL,
   `mensaje` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `user`
+--
+
+INSERT INTO `user` (`id`, `nombre`, `direccion`, `telefono`, `correo`, `mensaje`) VALUES
+(1, 'camila', 'calle', '2535623', 'camimcp21@gmail.com', 'holi'),
+(2, 'camila', 'calle', '2535623', 'camimcp21@gmail.com', 'camilita'),
+(3, 'camila', 'calle', '2535623', 'camimcp21@gmail.com', 'gty'),
+(4, 'camila', 'calle', '2535623', 'camimcp21@gmail.com', 'gty'),
+(5, 'camila', 'calle', '2535623', 'camimcp21@gmail.com', 'gty'),
+(6, 'camila', 'calle', '2535623', 'camimcp21@gmail.com', 'hola'),
+(7, 'camila', 'calle', '2535623', 'camimcp21@gmail.com', 'hola'),
+(8, 'camila', 'calle', '2535623', 'camimcp21@gmail.com', 'hola'),
+(9, 'camila', 'calle', '2535623', 'camimcp21@gmail.com', 'hola'),
+(10, 'camila', 'calle', '2535623', 'camimcp21@gmail.com', 'hola'),
+(11, 'camila', 'calle', '2535623', 'camimcp21@gmail.com', 'hola'),
+(12, 'camila', 'calle', '2535623', 'camimcp21@gmail.com', 'hola'),
+(13, 'camila', 'calle', '2535623', 'camimcp21@gmail.com', 'we');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
